accecf8fd5d3611d4d95acf3d1724189be73a59b
Victor Menezes <victormenezez15@gmail.com>
2021-03-24 05:44:44 -0300

    A simple API (really, really simple) to save videos with id, name, likes and views

---
+++ videoapi/app.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
+
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'likes': fields.Integer,
+    'views': fields.Integer
+}
+
+class Video(Resource):
+    @marshal_with(resource_fields)
+    def get(self, video_id):
+        video = VideoModel.query.filter_by(id=video_id).first()
+        if not video:
+            abort(404, message="Could not find video with that id")
+
+        return video 
+    
+    @marshal_with(resource_fields)
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        video = VideoModel.query.filter_by(id=video_id).first()
+        if video:
+            abort(409, message="Video id already exists")
+
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        video = VideoModel.query.filter_by(id=video_id).first()
+
+        if not video:
+            abort(404, message="Video doens't exist, cannot update")
+
+        if args['name']:
+            video.name = args['name']
+        if args['views']:
+            video.views = args['views']
+        if args['likes']:
+            video.likes = args['likes']
+        
+        db.session.commit()
+
+        return video
+
+    def delete(self, video_id):
+        video = VideoModel.query.filter_by(id=video_id).first()
+        if not video:
+            abort(404, message="Could not find video with that id")
+
+        db.session.delete(video)
+        db.session.commit()
+        
+        return 'Video deleted', 204
+
+
+api.add_resource(Video, '/video/<int:video_id>')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
